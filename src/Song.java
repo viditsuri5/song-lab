adb820dd44113dfa792d7268b72097ff77a3f872
Shyam-2007 <shyam.pb2007@gmail.com>
2024-10-25 22:04:23 +0000

    Initial commit

--- src/Song.java
+++ src/Song.java
@@ -3,22 +3,37 @@ public class Song
   // Private instance variables
   private String title = "Unknown";
   private String artist = "Unknown Artist";
-
+  private String date = "Unknown Date";
+  private String genre = "Unknown genre";
+  private String len = "Unknown Length";
+  private String Shake_the_audience = "Unknown";
+  private String obscene = "Unknown";
+  private String danceability = "Unknown Danceability";
+  private String loudness = "Unknown Loudness";
+  private String topic = "Unknown topic";
   // Default constructor
   public Song(){} 
-
+// shake the audience,obscene,danceability,loudness,topic
   // Overloaded constructor
-  public Song(String artist, String title) 
+  public Song(String artist, String title, String date, String genre, String len, String Shake_the_audience, String obscene, String danceability, String loudness, String topic) 
   {
     // note usage of this to assign to the instance variables
     this.artist = artist;
     this.title = title;
+    this.date = date;
+    this.len = len;
+    this.Shake_the_audience = Shake_the_audience;
+    this.obscene = obscene;
+    this.danceability = danceability;
+    this.loudness = loudness;
+    this.topic = topic;
+
   }
 
   // toString for printing the object
   public String toString()
   {
-    return artist + " - " + title;
+    return artist + " - " + title + " - "+date + " - "+ genre + " - "+ len +" - "+ Shake_the_audience+ " - "+ obscene+ " - "+danceability + " - "+ loudness+" - "+ topic;
   }
   
   // Getters for getting private instance variables
@@ -30,6 +45,38 @@ public class Song
   public String getArtist()
   {
     return artist;
+  }
+  public String getDate()
+  {
+    return date;
+  }
+  public String getGenre()
+  {
+    return genre;
+  }
+  public String getLen()
+  {
+    return len;
+  }
+  public String getShake_the_audience()
+  {
+    return Shake_the_audience;
+  }
+  public String getObscene()
+  {
+    return obscene;
+  }
+  public String getDanceability()
+  {
+    return danceability;
+  }
+  public String getLoudness()
+  {
+    return loudness;
+  }
+  public String getTopic()
+  {
+    return topic;
   }
   
   // Setters to update instance variables
