8e2e350635bc4a547fcec34144cf1cc56ab5ed87
viditsuri5 <viditsuri5@gmail.com>
2024-11-13 09:12:29 -0800

    Update SongImporter.java

--- src/SongImporter.java
+++ src/SongImporter.java
@@ -1,75 +1,43 @@
 import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
 
-public class SongImporter
-{
-    // SongImporter does not store any data, use "static"
-    // This method opens the file given by the parameter filename.
-    // Then it reads the data and breaks the lines up based on commas into a series of fields.
-    // Those fields are then used to create Song objects
-    public static ArrayList<Song> importSongsFromCSV(String filename)
-    {
-        ArrayList<Song> songs = new ArrayList<Song>();
+public class SongImporter {
+    public static ArrayList<Song> importSongsFromCSV(String filename) {
+        ArrayList<Song> songs = new ArrayList<>();
         int lineNumber = 0;
 
-        // The code in this "try" block will run. Any errors will get handled by the "catch" below. 
         try {
-            // Create a File object from the filename
             File file = new File(filename);
-            
-            // Create a Scanner object, this will read from the file.
-            Scanner csv = new Scanner(file);
+            Scanner csvReader = new Scanner(file);
 
-            // First line is a header - print it to see what it looks like.
-            // artist_name, track_name, release_date, genre, len, shake the audience, obscene, danceability, loudness, topic
-            String headerFields = csv.nextLine();
-            System.out.println("Header fields: " + headerFields);
+            // Read header line
+            String header = csvReader.nextLine();
+            System.out.println("Header fields: " + header);
 
-            // This while loop will loop until there are no more lines left in the CSV file.
-            while (csv.hasNextLine()) 
-            {
-                lineNumber++; // Keep track of line numbers to make inspection of data errors simpler.
+            // Read each line in the CSV file
+            while (csvReader.hasNextLine()) {
+                lineNumber++;
 
-                // Limit to 30 lines for testing (remove when you are testing real files)
-                if (lineNumber > 300000000)
-                {
-                    break;
-                }
-
-                // Read one line from the CSV file and store it in the variable "line"
-                String line = csv.nextLine();
-
-                // The string is then split on each comma into an array of fields.
+                String line = csvReader.nextLine();
                 String[] fields = line.split(",");
 
-                // Each field is given a name to simply their usage in constructing songs. 
-                String artistName = fields[0];
-                String trackName = fields[1];
-                String releaseDate = fields[2];
+                String artist = fields[0];
+                String title = fields[1];
+                String date = fields[2];
                 String genre = fields[3];
                 String length = fields[4];
-                String shakeTheAudience = fields[5];
-                String obscene = fields[6];
+                String shakeability = fields[5];
+                String obscenity = fields[6];
                 String danceability = fields[7];
                 String loudness = fields[8];
                 String topic = fields[9];
 
-                // Debug print
-                //System.out.println("artist: " + artist + " length: " + length + "topic: " + topic);
-
-                // Replace the next line with your song constructor
-                Song song = new Song(artistName, trackName, releaseDate, genre, length, shakeTheAudience, obscene, danceability, loudness, topic); // code to construct a song object
-
-                // Add the song object to the data output. (Leave alone!~)
+                Song song = new Song(artist, title, date, genre, length, shakeability, obscenity, danceability, loudness, topic);
                 songs.add(song);
             }
-            csv.close();
+            csvReader.close();
         } catch (Exception ex) {
-            // If there is an error, this code will run
             System.out.println("Error on line: " + lineNumber);
             ex.printStackTrace();
         }
