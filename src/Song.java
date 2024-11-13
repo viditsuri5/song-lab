8e1fa6d9b697786720cfd56ee18b987cf90128d5
viditsuri5 <viditsuri5@gmail.com>
2024-11-13 09:11:38 -0800

    Update Song.java

--- src/Song.java
+++ src/Song.java
@@ -1,125 +1,58 @@
-public class Song
-{
-  // Private instance variables
-  private String title = "Unknown";
-  private String artist = "Unknown Artist";
-  private String date = "Unknown Date";
-  private String genre = "Unknown genre";
-  private String len = "Unknown Length";
-  private String Shake_the_audience = "Unknown";
-  private String obscene = "Unknown";
-  private String danceability = "Unknown Danceability";
-  private String loudness = "Unknown Loudness";
-  private String topic = "Unknown topic";
-  // Default constructor
-  public Song(){} 
-// shake the audience,obscene,danceability,loudness,topic
-  // Overloaded constructor
-  public Song(String artist, String title, String date, String genre, String len, String Shake_the_audience, String obscene, String danceability, String loudness, String topic) 
-  {
-    // note usage of this to assign to the instance variables
-    this.artist = artist;
-    this.title = title;
-    this.date = date;
-    this.len = len;
-    this.Shake_the_audience = Shake_the_audience;
-    this.obscene = obscene;
-    this.danceability = danceability;
-    this.loudness = loudness;
-    this.topic = topic;
+public class Song {
+    private String title = "Unknown";
+    private String artist = "Unknown Artist";
+    private String date = "Unknown Date";
+    private String genre = "Unknown Genre";
+    private String length = "Unknown Length";
+    private String shakeability = "Unknown";
+    private String obscenity = "Unknown";
+    private String danceability = "Unknown Danceability";
+    private String loudness = "Unknown Loudness";
+    private String topic = "Unknown Topic";
 
-  }
+    // Default constructor
+    public Song() {}
 
-  // toString for printing the object
-  public String toString()
-  {
-    return artist + " - " + title + " - "+ date + " - "+ genre + " - "+ len +" - "+ Shake_the_audience+ " - "+ obscene+ " - "+danceability + " - "+ loudness+" - "+ topic;
-  }
-  
-  // Getters for getting private instance variables
-  public String getTitle()
-  {
-    return title;
-  }
-  
-  public String getArtist()
-  {
-    return artist;
-  }
-  public String getDate()
-  {
-    return date;
-  }
-  public String getGenre()
-  {
-    return genre;
-  }
-  public String getLen()
-  {
-    return len;
-  }
-  public String getShake_the_audience()
-  {
-    return Shake_the_audience;
-  }
-  public String getObscene()
-  {
-    return obscene;
-  }
-  public String getDanceability()
-  {
-    return danceability;
-  }
-  public String getLoudness()
-  {
-    return loudness;
-  }
-  public String getTopic()
-  {
-    return topic;
-  }
-  
-  // Setters to update instance variables
-  public void setTitle(String title)
-  {
-      this.title = title;
-  }
-  
-  public void setArtist(String artist)
-  {
-    this.artist = artist;
-  }
-  public void setDate(String date)
-  {
-      this.date = date;
-  }
-  public void setGenre(String genre)
-  {
-      this.genre = genre;
-  }
-  public void setLen(String len)
-  {
-      this.len = len;
-  }
-  public void setShake_the_audience(String Shake_audience)
-  {
-      this.Shake_the_audience = Shake_audience;
-  }
-  public void setObscene(String obscene)
-  {
-      this.obscene = obscene;
-  }
-  public void setDanceability(String danceability)
-  {
-      this.danceability = danceability;
-  }
-  public void setLoudness(String loudness)
-  {
-      this.loudness = loudness;
-  }
-  public void setTopic(String topic)
-  {
-      this.topic = topic;
-  }
+    // Overloaded constructor
+    public Song(String artist, String title, String date, String genre, String length, String shakeability, String obscenity, String danceability, String loudness, String topic) {
+        this.artist = artist;
+        this.title = title;
+        this.date = date;
+        this.genre = genre;
+        this.length = length;
+        this.shakeability = shakeability;
+        this.obscenity = obscenity;
+        this.danceability = danceability;
+        this.loudness = loudness;
+        this.topic = topic;
+    }
 
+    // toString for printing
+    public String toString() {
+        return artist + " - " + title + " - " + date + " - " + genre + " - " + length + " - " + shakeability + " - " + obscenity + " - " + danceability + " - " + loudness + " - " + topic;
+    }
+
+    // Getters
+    public String getTitle() { return title; }
+    public String getArtist() { return artist; }
+    public String getDate() { return date; }
+    public String getGenre() { return genre; }
+    public String getLen() { return length; }
+    public String getShake_the_audience() { return shakeability; }
+    public String getObscene() { return obscenity; }
+    public String getDanceability() { return danceability; }
+    public String getLoudness() { return loudness; }
+    public String getTopic() { return topic; }
+
+    // Setters
+    public void setTitle(String title) { this.title = title; }
+    public void setArtist(String artist) { this.artist = artist; }
+    public void setDate(String date) { this.date = date; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public void setLen(String length) { this.length = length; }
+    public void setShake_the_audience(String shakeability) { this.shakeability = shakeability; }
+    public void setObscene(String obscenity) { this.obscenity = obscenity; }
+    public void setDanceability(String danceability) { this.danceability = danceability; }
+    public void setLoudness(String loudness) { this.loudness = loudness; }
+    public void setTopic(String topic) { this.topic = topic; }
 }
